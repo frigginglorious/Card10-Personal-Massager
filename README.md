8e040303886424ed03aa8b242f8eac544f255965
dkraft <dkraft@camsbycbs.com>
2019-08-26 14:17:26 -0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Card10-Personal-Massager
 Testing the vibrate motor on Card10 badge from CCCamp2019
+
+
+## Main Repos
+https://git.card10.badge.events.ccc.de
+
+## Main Info
+https://card10.badge.events.ccc.de/
+
+## API Doc
+https://firmware.card10.badge.events.ccc.de/epicardium/api.html#vibration-motor
---
+++ __init__.py
@@ -0,0 +1,43 @@
+import vibra
+import display
+import utime
+import buttons
+
+vibration_speed = 10
+disp = display.open()
+
+def init():
+    vibration_speed = 10
+
+def headline():
+    # disp.print("Personal \n Massager", posy=0, fg=[0, 255, 255])
+    # disp.print("Left=Lower", posy=-10, fg=[0, 255, 255])
+    # stuff_in_string = f"Vibe Speed \n  {vibration_speed}."
+    stuff_in_string = "Vibe Speed \n  {vibration_speed}."
+    disp.print(stuff_in_string, posy=0, fg=[0, 255, 255])
+
+def goSlower():
+    if vibration_speed > 1:
+        vibration_speed = vibration_speed - 1
+
+def goFaster():
+    vibration_speed = vibration_speed + 1
+
+
+
+while True:
+    disp.clear()
+    headline()
+    v = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT)
+    if v == 0:
+        button_pressed = False
+    if not button_pressed and v & buttons.BOTTOM_LEFT != 0:
+        button_pressed = True
+        goSlower()
+    if not button_pressed and v & buttons.BOTTOM_RIGHT != 0:
+        button_pressed = True
+        goFaster()
+
+
+    vibra.vibrate(100)
+    utime.sleep_ms(vibration_speed)
